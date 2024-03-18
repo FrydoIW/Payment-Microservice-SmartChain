131da69cdae0075061d5397aafaee00d0203e116
Frydo Ichmar Wienanta <sologankerfrydo99@gmail.com>
2024-03-18 23:18:02 +0700

    First Initialize

---
+++ src/test/java/microservices/fullstack/payment/Payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.fullstack.payment.Payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
